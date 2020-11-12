d5ebd700211caeae9d42d455c8304eb92a6242d6
david connaughton <davidconnaughton@davids-MacBook-Pro.local>
2020-11-12 21:24:11 +0000

    Added Delete Functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
@@ -26,6 +26,13 @@ def home():
 def concert_list():
     concerts = mongo.db.artists.find()
     return render_template("concerts.html", concerts=concerts)
+
+
+@app.route("/delete/<artist_id>")
+def delete_artist(artist_id):
+    mongo.db.artists.remove({"_id": ObjectId(artist_id)})
+    flash("You have deleted a Concert")
+    return redirect(url_for("concert_list"))
 
 
 if __name__ == "__main__":
